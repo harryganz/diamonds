f8c0fa22b8b1335cc78159ff26ac62c4407cc88b
Harry Ganz <harryyehudah.ganz@gartner.com>
2017-07-11 15:48:51 -0400

    Quick console application prototype scraper
    * Scrapes first page and prints to console

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
   "log"
   "fmt"
   "net/url"
+  "net/http"
   "strconv"
+  "io/ioutil"
+  "bytes"
+  "strings"
 
   "github.com/PuerkitoBio/goquery"
 )
@@ -64,11 +68,62 @@ func main () {
   // Append the query to the URL
   u.RawQuery = q.Encode()
 
-  // Request the URL and get the returned document
-  doc, err := goquery.NewDocument(u.String())
+  // Request the URL and get the response
+  resp, err := http.Get(u.String())
   if err != nil {
     log.Fatal(err)
   }
 
-  fmt.Print(doc.Text())
+  // Appends html, body, and table tags around returned data because
+  // parser cannot handle tr tags not in a table
+  // TODO use a writer to append instead so that this can be done
+  // asynchronously
+  body, err := ioutil.ReadAll(resp.Body)
+  if err = resp.Body.Close(); err != nil {
+    log.Fatal(err)
+  }
+  body = append([]byte("<html><body><table><tbody>"), body...)
+  body = append(body, []byte("</tbody></table><body></html>")...)
+
+  // Parse document with goquery
+  doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Printf("%10s %10s %10s %10s %10s %10s %10s %10s %20s %10s\n",
+  "Shape", "Carat", "Cut", "Color", "Clarity",
+  "Table", "Depth", "Cert", "Dimensions", "Price")
+  doc.Find("tr").Each(func(i int, row *goquery.Selection) {
+    diamond := make(map[string]string)
+    row.Find("td").Each(func(k int, cell *goquery.Selection) {
+      switch k {
+      case 1:
+        diamond["shape"] = cell.Text()
+      case 2:
+        diamond["carat"] = cell.Text()
+      case 3:
+        diamond["cut"] = cell.Text()
+      case 4:
+        diamond["color"] = cell.Text()
+      case 5:
+        diamond["clarity"] = cell.Text()
+      case 6:
+        diamond["table"] = cell.Text()
+      case 7:
+        diamond["depth"] = cell.Text()
+      case 8:
+        diamond["cert"] = cell.Text()
+      case 9:
+        diamond["dimensions"] = cell.Text()
+      case 10:
+        diamond["price"] = strings.Replace(cell.Text(), "$", "", 1)
+      }
+    })
+
+    fmt.Printf("%10s %10s %10s %10s %10s %10s %10s %10s %20s %10s\n",
+      diamond["shape"], diamond["carat"], diamond["cut"], diamond["color"],
+      diamond["clarity"], diamond["table"], diamond["depth"], diamond["cert"],
+      diamond["dimensions"], diamond["price"])
+  })
 }
