8a1313f957b81a117e974b53fcb30582e3fd3a37
Harry Ganz <harryyehudah.ganz@gartner.com>
2017-07-12 15:53:05 -0400

    Creates diamonds package

---
+++ diamonds.go
@@ -0,0 +1,5 @@
+// The diamonds package scrapes the diamond search engine,
+// http://diamondse.info, for data on diamond pricing
+// it can then export this data to CSV format for easy analysis
+// in statistical programming langauges such as R
+package diamonds
