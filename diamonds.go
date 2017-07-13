d037259d2427457f91fcc79352c3697e2a18d7b6
Harry Ganz <hganz@harryganz.com>
2017-07-12 22:21:48 -0400

    Changes documentation format

--- diamonds.go
+++ diamonds.go
@@ -1,4 +1,4 @@
-// The diamonds package scrapes the diamond search engine,
+// Package diamonds scrapes the diamond search engine,
 // http://diamondse.info, for data on diamond pricing
 // it can then export this data to CSV format for easy analysis
 // in statistical programming langauges such as R
