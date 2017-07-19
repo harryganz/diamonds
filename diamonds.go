2203195928f89e8572dae1a486f413e775259515
Harry Ganz <harryyehudah.ganz@gartner.com>
2017-07-19 12:44:17 -0400

    Restart project

--- diamonds.go
+++
@@ -1,5 +0,0 @@
-// Package diamonds scrapes the diamond search engine,
-// http://diamondse.info, for data on diamond pricing
-// it can then export this data to CSV format for easy analysis
-// in statistical programming langauges such as R
-package diamonds
